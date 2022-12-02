a64f647c11724b4cb06dcc61725939df0dbf9b7c
Om N. Dabhi <93876134+omndabhi@users.noreply.github.com>
2022-12-02 11:11:36 +0530

    Add files via upload
    Calculating Time Complexity of Factorial Program

---
+++ Factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<time.h>
+
+void main (){
+    clock_t start, end;
+    double cpu_time_used;
+    int n,ans=1;
+
+    printf("Enter Number:");
+    scanf("%d",&n);
+
+    start = clock();
+    for(int i=1;n>=i;i++){
+         ans=ans*i; 
+    }
+    end = clock();
+
+    printf("Factorial = %d",ans);
+    cpu_time_used = ((double)(end-start))/CLOCKS_PER_SEC;
+    printf("Time Taken by Processor :%f ",cpu_time_used);
+}
